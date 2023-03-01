af5a2f79c20e271896d4459001ecac3e34164d25
VoThanhKha <khavtgcc200092@fpt.edu.vn>
2023-03-01 22:19:02 +0700

    Create a product view page for admin

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Manager\CartManager;
+use App\Manager\Manager;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
+/**
+ * @Route("/product")
+ */
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    private ProductRepository $repo;
+    public function __construct(ProductRepository $repo)
+   {
+      $this->repo = $repo;
+   }
+    /**
+     * @Route("/show", name="product_show")
+     */
+    public function readAllAction(): Response
     {
+        $products = $this->repo->findAll();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products'=>$products
         ]);
     }
+
+     /**
+     * @Route("/{id}", name="product_read",requirements={"id"="\d+"})
+     */
+    public function showAction(Product $p): Response
+    {
+        return $this->render('detail.html.twig', [
+            'p'=>$p
+        ]);
+
+    }
+
+
+  
 }
