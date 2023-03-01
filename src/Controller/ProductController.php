b231e8154fc397a82f4f2f28e7607b7654797e83
VoThanhKha <khavtgcc200092@fpt.edu.vn>
2023-03-01 22:08:32 +0700

    Complete the login and registration function

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Manager\CartManager;
+use App\Manager\Manager;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
+/**
+ * @Route("/product")
+ */
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    private ProductRepository $repo;
+    public function __construct(ProductRepository $repo)
+   {
+      $this->repo = $repo;
+   }
+    /**
+     * @Route("/show", name="product_show")
+     */
+    public function readAllAction(): Response
     {
+        $products = $this->repo->findAll();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products'=>$products
         ]);
     }
+
+     /**
+     * @Route("/{id}", name="product_read",requirements={"id"="\d+"})
+     */
+    public function showAction(Product $p): Response
+    {
+        return $this->render('detail.html.twig', [
+            'p'=>$p
+        ]);
+
+    }
+
+     /**
+     * @Route("/add", name="product_create")
+     */
+    public function createAction(Request $req, SluggerInterface $slugger): Response
+    {
+        
+        $p = new Product();
+        $form = $this->createForm(ProductType::class, $p);
+
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+            if($p->getCreated()===null){
+                $p->setCreated(new \DateTime());
+            }
+            $imgFile = $form->get('file')->getData();
+            if ($imgFile) {
+                $newFilename = $this->uploadImage($imgFile,$slugger);
+                $p->setImage($newFilename);
+            }
+            $this->repo->save($p,true);
+            $this->addFlash('success','You have successfully updated your profile!');
+            return $this->redirectToRoute('product_create', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render("product/form.html.twig",[
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+    public function uploadImage($imgFile, SluggerInterface $slugger): ?string{
+        $originalFilename = pathinfo($imgFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $slugger->slug($originalFilename);
+        $newFilename = $safeFilename.'-'.uniqid().'.'.$imgFile->guessExtension();
+        try {
+            $imgFile->move(
+                $this->getParameter('image_dir'),
+                $newFilename
+            );
+        } catch (FileException $e) {
+            echo $e;
+        }
+        return $newFilename;
+    }
+
+  
 }
